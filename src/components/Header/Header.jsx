e51c43fd0623466ceafa621020ad878bad94f6f5
guillermogm <guillermogm009@gmail.com>
2024-07-19 09:55:34 +0200

    feat: register connected with server

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+
 import { CSurfer } from '../CSurfer/CSurfer'
 
 export const Header = () => {
-  const navigate = useNavigate()
 
   return (
     <>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -2,41 +2,58 @@ import React, { useState } from 'react'
 import { Cinput } from '../Cinput/Cinput'
 
 export const CRegister = () => {
-    const [credentials, setCredentials] = useState(
-        {
-          email:"",
-          password:""
-        }
-      )
-      const handleChange= (e)=>{
-        setCredentials(prevState =>(
-          {
-            ...prevState,
-            [e.target.name]: e.target.value
-          }
-        ))
+  const [credentials, setCredentials] = useState(
+    {
+      email: "",
+      password: ""
+    }
+  )
+  const handleChange = (e) => {
+    console.log(1)
+    setCredentials(prevState => (
+      {
+        ...prevState,
+        [e.target.name]: e.target.value
       }
-    
-    
-      const register= ()=>{
-        if(credentials.email === "" || credentials.password === ""){
-            return console.log("No Email or Password");
-        }
-        console.log("Register");
-        console.log(credentials)
+    ))
+  }
+
+
+  const register = async () => {
+    try {
+      console.log(credentials)
+      // validar la data voy a enviar
+      if (credentials.email === "" || credentials.password === "") {
+        return console.log("No Email or Password");
       }
+      // consumir la api
+      const request = await fetch("http://localhost:4000/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+      });
+  
+      const result = await request.json();
+  
+      console.log(result);   
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <>
-    <h1>Register</h1>
+      <h1>Register</h1>
       <div>
         {/* <label htmlFor="email">Email</label> */}
         {/* <input type="email" name="email" placeholder="Email" onChange={handleChange}></input> */}
-        <Cinput type="email" name="email" placeholder="Email" emitFuntion={handleChange}/>
+        <Cinput type="email" name="email" placeholder="Email" emitFunction={handleChange} />
       </div>
       <div>
         {/* <label htmlFor="password">Password</label> */}
         {/* <input type="password" name="password" placeholder="Password" onChange={handleChange}></input> */}
-        <Cinput type="password" name="password" placeholder="Password" emitFuntion={handleChange}/>
+        <Cinput type="password" name="password" placeholder="Password" emitFunction={handleChange} />
       </div>
       <div>
         <input type="button" name="send" value="Register" onClick={register}></input>
